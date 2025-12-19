4c9428dd30cd55ad869b855f879e701cc5371253
Frank Boucher <fboucher@outlook.com>
2025-12-19 11:14:59 -0500

    upgrade in progress

--- src/AppHost/Program.cs
+++ src/AppHost/Program.cs
@@ -3,18 +3,19 @@ var builder = DistributedApplication.CreateBuilder(args);
 var mysql = builder.AddMySql("sqlsvr2d6")
                    //.WithPhpMyAdmin()
                    .WithLifetime(ContainerLifetime.Persistent);
-                
+
 var db2d6 = mysql.AddDatabase("db2d6");
 
-mysql.WithInitBindMount(source: "../../database/scripts", isReadOnly: false);
+//mysql.WithInitBindMount(source: "../../database/scripts", isReadOnly: false);
+mysql.WithInitFiles("../../database/scripts");
 
 var dab = builder.AddDataAPIBuilder("dab", ["../../database/dab-config.json"])
                 .WithReference(db2d6)
                 .WaitFor(db2d6);
 
-builder.AddProject<Projects._2d6_dungeon_web_client>("webapp") 
+builder.AddProject<Projects._2d6_dungeon_web_client>("webapp")
         .WaitFor(dab)
         .WithReference(dab)
-        .WithExternalHttpEndpoints(); 
+        .WithExternalHttpEndpoints();
 
 builder.Build().Run();
